039b84010c82bccc7bb3d4c3e264e0d006af0d56
Kyle Carhart <KMCGamer@live.com>
2017-10-24 20:34:56 -0400

    branch commit

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          I made a change to branch kcarhart.
         </Text>
         <Text style={styles.instructions}>
           I have made my presence known. FEAR ME!!!
