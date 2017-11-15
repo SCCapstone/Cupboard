f36bcdebb271d2b66a10db089d19e7f4d6b42f63
WillWells <wellswc@email.sc.edu>
2017-11-14 21:58:00 -0500

    Remove unneccessary code

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -30,93 +24,6 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: this.props.credential
-    };
-  }
-
-  tempfunction(text) {
-    if (this.props.credential === "Username") {
-      this.props.self.setState({username: text});
-    } else {
-      this.props.self.setState({password: text});
-    }
-  }
-
-  render() {
-    return (
-      <TextInput
-        style={{height: 40, width: '75%', paddingLeft: 10, marginBottom: 10, borderColor: 'lightgray', borderWidth: 1}}
-        onChangeText={(text) => {
-            this.setState({text});
-            this.tempfunction(text);
-          }
-        }
-        onFocus={(text) => this.setState({text: ''})}
-        value={this.state.text}
-        secureTextEntry={this.props.secure}
-      />
-    );
-  }
-}
-
-//export default class App extends Component<{}> {
-class App extends Component<{}> {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: '',
-      password: ''
-    }
-  }
-
-  signIn() {
-    firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password)
-      .then((user) => {
-        Alert.alert('Signed in successfully!');
-      }).catch( (err) => {
-
-        Alert.alert('Incorrect log in!');
-    });
-  }
-
-  createUser() {
-    firebase.auth().createUserWithEmailAndPassword(this.state.username, this.state.password)
-      .then((user) => {
-        Alert.alert('Created new account!');
-      }).catch( (err) => {
-        console.log(err);
-        Alert.alert('Unable to create account.');
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        </Text>
-        <SignIn credential={"Username"} self={this} secure={false}/>
-        <SignIn credential={"Password"} self={this} secure={true}/>
-        <Button
-          onPress={this.signIn.bind(this)}
-          title="Sign in"
-          color="darkblue"
-        />
-        <Button
-          onPress={this.createUser.bind(this)}
-          title="Create account"
-          color="lightgreen"
-        />
-      </View>
-    );
-  }
-}
 
 //make sure this is near the bottom
 const RootNavigator = StackNavigator({
