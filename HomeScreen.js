5cd2974a73f7d1e2d36d9aa4ba00302350a690f2
Zakery L Slater <slaterz@email.sc.edu>
2017-12-07 17:13:42 -0500

    style change

--- HomeScreen.js
+++ HomeScreen.js
@@ -9,6 +9,9 @@ import {
 import { Button } from 'react-native-elements';
 import { style } from "./Styles";
 
+const remote = 'http://www.retinaiphonewallpapers.com/wp-content/uploads/00023.jpg';
+
+
 export default class HomeScreen extends Component<{}> {
 
   constructor(props) {
@@ -18,12 +21,21 @@ export default class HomeScreen extends Component<{}> {
   render() {
     const navigation = this.props.navigation;
     const fbhandler = navigation.state.params.fbhandler;
-    const resizeMode = 'center';
+    const resizeMode = 'cover';
 
 
     return (
       <View style={style.containerCenterContent}>
-              /*TODO fix img
+        <View
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            //TODO fix img
               <Image
                   style={{
                       flex: 1,
@@ -31,48 +43,63 @@ export default class HomeScreen extends Component<{}> {
                   }}
                   source={{ uri: remote }}
               />
-              */
+        </View>
+        <View>
         <Text>Hello {fbhandler.user.email}!</Text>
-        <Button
-          containerViewStyle={style.buttonContainer}
-          buttonStyle={style.button}
-          title='MY CUPBOARD'
-          onPress={() => {
-            navigation.navigate("CupboardS", {
-              'fbhandler': fbhandler
-            });
-          }}
-        />
-        <Button
-          title='RECIPES'
-          containerViewStyle={style.buttonContainer}
-          buttonStyle={style.button}
-          onPress={() => {
-            navigation.navigate("RecipesS");
-          }}
-        />
-        <Button
-          title='SHOPPING LISTS'
-          containerViewStyle={style.buttonContainer}
-          buttonStyle={style.button}
-          onPress={() => {
-            navigation.navigate("ListsS", {
-              'fbhandler': fbhandler
-            });
-          }}
-        />
-        <Button
-          title='SIGN OUT'
-          containerViewStyle={style.logoutButtonContainer}
-          buttonStyle={style.button}
-          onPress={()=>{
-            fbhandler.signOut(()=>{
-              navigation.goBack();
-            }, (err)=> {
-              Alert.alert(err);
-            });
-          }}
-        />
+        </View>
+        <View style={style.buttons}>
+          <Button
+            containerViewStyle={style.buttonContainer}
+            buttonStyle={style.button}
+            backgroundColor="#b8b09b"
+            title="MY CUPBOARD"
+            color="black"
+            raised
+            onPress={() => {
+              navigation.navigate("CupboardS", {
+                'fbhandler': fbhandler
+              });
+            }}
+          />
+          <Button
+            containerViewStyle={style.buttonContainer}
+            buttonStyle={style.button}
+            backgroundColor="#b8b09b"
+            title="RECIPES"
+            color="black"
+            raised
+            onPress={() => {
+              navigation.navigate("RecipesS");
+            }}
+          />
+          <Button
+            containerViewStyle={style.buttonContainer}
+            buttonStyle={style.button}
+            backgroundColor="#b8b09b"
+            title="SHOPPING LISTS"
+            color="black"
+            onPress={() => {
+              navigation.navigate("ListsS", {
+                'fbhandler': fbhandler
+              });
+            }}
+          />
+          <Button
+            containerViewStyle={style.logoutButtonContainer}
+            buttonStyle={style.button}
+            backgroundColor="#dbd7cc"
+            title="SIGN OUT"
+            color="black"
+            raised
+            onPress={()=>{
+              fbhandler.signOut(()=>{
+                navigation.goBack();
+              }, (err)=> {
+                Alert.alert(err);
+              });
+            }}
+          />
+        </View>
       </View>
     );
   }
